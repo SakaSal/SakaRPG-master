cf8a871accbdb2ca33e09cb5e7752371c4587522
SakaSal <SakaSal@SakaMail.cum>
2025-02-20 14:34:59 -0800

    PROGRESS. created a pad that shows a "map" that can be navigated in the terminal.

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -1,4 +1,5 @@
-import curses, time
+import curses
+import time
 from curses import wrapper
 
 
@@ -14,28 +15,53 @@ def main(stdscr):
 
     stdscr.clear()
     # numbers are row and column
-    '''
+    """
     stdscr.addstr(10, 10, "hello world", GREEN_AND_BLACK)
     stdscr.addstr(10, 13, "overwrite")
     stdscr.addstr(12, 20, "goodbye world", RED_AND_WHITE | curses.A_BOLD)
     stdscr.refresh()
     stdscr.getch()
-    '''
+    """
 
     pad = curses.newpad(100, 100)
     stdscr.refresh()
 
-    for i in range(0,99):
-        for j in range(0,99):
-            pad.addstr(".", GREEN_AND_BLACK)
+    for i in range(0, 50):
+        for j in range(0, 50):
+            pad.addstr("._", GREEN_AND_BLACK)
 
     # (curses.LINES -1, curses.ROWS - 1) coordinates of the screen
     # (padrow, padcol, topwinrow, topwincol, botwinrow, botwincol)
     stdscr.clear()
-    stdscr.refresh()
-    pad.refresh(0, 0, 5, 5, 15, 25)
-    time.sleep(0.02)
-    stdscr.getch()
+    count = 0
+    for i in range(20):
+        if count > 9:
+            count = 0
+        stdscr.addstr(str(count), GREEN_AND_BLACK)
+        count += 1
+
+    for i in range(25):
+        stdscr.addstr(f"{i}\n", GREEN_AND_BLACK)
+        stdscr.refresh()
+
+    y, x = (10, 10)
+    while True:
+        try:
+            key = stdscr.getkey()
+        except:
+            key = None
+
+        if key == "KEY_LEFT":
+            x -= 1
+        elif key == "KEY_RIGHT":
+            x += 1
+        elif key == "KEY_UP":
+            y -= 1
+        elif key == "KEY_DOWN":
+            y += 1
+
+        pad.addstr(y, x, "0", GREEN_AND_BLACK)
+        pad.refresh(y - 5, x - 5, 10, 10, 20, 20)
 
 
 wrapper(main)

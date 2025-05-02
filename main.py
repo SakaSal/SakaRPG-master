4f093569950b8b52b33e453c4522f165c55589f2
SakaSal <SakaSal@SakaMail.cum>
2025-05-02 16:11:01 -0700

    starting the fight method

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from gear import machete
 from races import Goblin, Human
 
 sal = Goblin("sal")
-# jules = Human("jules")
-print(sal.atribs["attack"])
+jules = Human("jules")
+# print(sal.atribs["attack"])
 sal.equip_item(machete, "right_hand")
-print(sal.atribs["attack"])
+sal.fight(jules)
+# print(sal.atribs["attack"])
 # sal.atribs.update({"attack": 5})
 # print(sal.atribs["attack"])
 # print(jules.atribs)

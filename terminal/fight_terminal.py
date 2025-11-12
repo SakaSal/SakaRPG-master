b5edd705081b87f13625edb5584122d7068e486e
SakaSal <SakaSal@SakaMail.cum>
2025-11-12 15:58:03 -0800

    worked on the terminal some more.
    some how breaks after first attack.
    
    is not able to actually change charachter
    HP as well.

--- terminal/fight_terminal.py
+++ terminal/fight_terminal.py
@@ -14,7 +14,6 @@ def fight_terminal(stdscr, attacker, defender):
     curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_RED)
     # color for fight
     curses.init_pair(4, curses.COLOR_RED, curses.COLOR_YELLOW)
-
     # set colors
     attacker_color = curses.color_pair(1)
     defender_color = curses.color_pair(3)
@@ -31,27 +30,63 @@ def fight_terminal(stdscr, attacker, defender):
     # get damage
     attacker_damage, defender_damage = return_damage(attacker, defender, "melee")
 
-    # battle prompt sans HP box
-    stdscr.addstr(1, 0, "[FIGHT]", fight_color)
-    stdscr.addstr(
-        3, 0, f"[ATTACKER]\nstrikes remaining: {attacker_strikes}\n", attacker_color
-    )
-    stdscr.addstr(
-        6, 0, f"[DEFENDER]\nstrikes remaining: {defender_strikes}\n", defender_color
-    )
+    # set the in_fight variable
+    in_fight = True
 
-    # add HP boxes
-    hp_win_attacker = curses.newwin(1, attacker_hp, 3, 11)
-    hp_win_attacker.bkgd("=", hp_color)
-    hp_win_attacker.addstr(f" HP: {attacker_hp}")
+    # start fight loop
+    while in_fight:
+        # battle prompt sans HP box
+        stdscr.addstr(1, 0, "[FIGHT]", fight_color)
+        stdscr.addstr(
+            3, 0, f"[ATTACKER]\nstrikes remaining: {attacker_strikes}\n", attacker_color
+        )
+        stdscr.addstr(
+            6, 0, f"[DEFENDER]\nstrikes remaining: {defender_strikes}\n", defender_color
+        )
 
-    hp_win_defender = curses.newwin(1, defender_hp, 6, 11)
-    hp_win_defender.bkgd("=", hp_color)
-    hp_win_defender.addstr(f" HP: {defender_hp}")
+        # add HP boxes
+        hp_win_attacker = curses.newwin(1, attacker_hp, 3, 11)
+        hp_win_attacker.bkgd("=", hp_color)
+        hp_win_attacker.addstr(f" HP: {attacker_hp}")
 
-    # refresh the windows
+        hp_win_defender = curses.newwin(1, defender_hp, 6, 11)
+        hp_win_defender.bkgd("=", hp_color)
+        hp_win_defender.addstr(f" HP: {defender_hp}")
+
+        # fight bar
+        fight_bar = curses.newwin(1, 20, 8, 0)
+        fight_bar.bkgd("+", fight_color)
+
+        # refresh the windows
+        screens = [stdscr, hp_win_attacker, hp_win_defender, fight_bar]
+        refresh_all(screens)
+
+        if attacker_strikes > 0:
+
+            fight_bar.addstr("[ATTACKER]")
+            fight_bar.refresh()
+            stdscr.addstr(9, 0, f"[1]:Attack [2] Parry\n", attacker_color)
+            choice = stdscr.getkey()
+            if choice == "1":
+                attacker_strikes -= 1
+                defender_hp -= attacker_damage
+                stdscr.addstr(
+                    12,
+                    0,
+                    f"{attacker.name} strikes a blow dealing {attacker_damage} dmg to {defender.name}, {defender.name}'s hp is {defender.atribs["hp"]}",
+                )
+
+    curses.endwin()
+
+
+def refresh_all(screens):
+    for screen in screens:
+        screen.refresh()
+
+
+"""
     stdscr.refresh()
     hp_win_attacker.refresh()
     hp_win_defender.refresh()
-
-    stdscr.getkey()
+    fight_bar.refresh()
+"""

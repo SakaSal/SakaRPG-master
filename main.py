739816842d8687e2a40f6666604f5f10f61a13cf
SakaSal <salvadormem@gmail.com>
2025-12-22 23:35:22 -0800

    optimizing the main menu

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from curses import wrapper
 
 from gear import combat_knife, fist, machete, gear_list
-from races import Goblin, Human
+from races import Goblin, Human, race_list
 from terminal import fight_terminal
 
 #sal = Goblin("sal")
@@ -13,7 +13,7 @@ from terminal import fight_terminal
 #jules.equip_item(fist, "right_hand")
 # print(jules.atribs)
 # print(sal.atribs)
-Team = []
+team = []
 
 def main():
     
@@ -27,39 +27,29 @@ def welcome():
     choice = input("Enter choice: ")
     
     if choice == "1":
-        
         points = 100
         team_size = 0
-        print(f"You have {points} points remaining.\n1: Recruit Goblin (10 pts)\n2:Recruit Human (10 pts)\n")
-        choice = input("Choice: ")
-        
-        if choice == "1":
-            
-            points -= 10
-            
-            while points > 5:
-                
-                print(f"You have {points} points remaining.")
-                name = input("Enter hero name: ")
-                hero = Goblin(name)
-                
-                print(f"select a weapon for {name}")
-                i = 1
-                choices = {}
-                for item in gear_list:
-                    print(f"{i}: {item.name}, ({item.points} pts) \n")
-                    choices[i] = item
-                    i += 1
-                choice = int(input("Choice: "))
-                hero.equip_item(choices[choice], "right hand")
-                points -= choices[choice].points
-                Team.append(hero)
-            
-        if choice == "2":
-            points -= 10
-            print(f"You have {points} points remaining.")
-            name = input("Enter character name: ")
-
-
+        print(f"You have {points} points remaining.\nRecruit\n")
+        i=1
+        races = {}
+        for race in race_list:
+            print(f"{i}: {race} (10 pts)")
+            races[i] = race
+            i += 1
+                        
+        choice = int(input("Choice: "))
+        name = input("Enter hero name: ")
+        hero = races[choice](name)
+        print(f"You have {points} points remaining.\nselect a weapon for {name}")
+        i = 1
+        weapons = {}
+        for weapon in gear_list:
+            print(f"{i}: {weapon.name}, ({weapon.points} pts) \n")
+            weapons[i] = weapon
+            i += 1
+        choice = int(input("Choice: "))
+        hero.equip_item(weapons[choice], "right hand")
+        points -= weapons[choice].points
+        team.append(hero)
 
 main()
--- races/__init__.py
+++ races/__init__.py
@@ -1,3 +1,4 @@
 from .base import Base, Being
 from .goblin import Goblin
 from .human import Human
+race_list = [Goblin, Human]
